4ef2473dacc2e778841f9b3a9a114dadf0d5df83
Edwin Cardenas <eacardenase@gmail.com>
2026-05-10 16:57:40 -0500

    Initial Commit

---
+++ GreenField/GreenFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenFieldApp.swift
+//  GreenField
+//
+//  Created by Edwin Cardenas on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct GreenFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
