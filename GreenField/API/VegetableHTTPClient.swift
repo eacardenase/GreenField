bb9ca859ba15cff777830a933007b31a7929b806
Edwin Cardenas <eacardenase@gmail.com>
2026-05-10 18:56:35 -0500

    feat: add initial model and project structure

---
+++ GreenField/API/VegetableHTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  VegetableHTTPClient.swift
+//  GreenField
+//
+//  Created by Edwin Cardenas on 5/10/26.
+//
+
+import Foundation
+
+struct VegetableHTTPClient {
+    func fetchVegetable() -> [Vegetable] {
+        return []
+    }
+}
---
+++ GreenField/Models/Vegetable.swift
@@ -0,0 +1,17 @@
+//
+//  Vegetable.swift
+//  GreenField
+//
+//  Created by Edwin Cardenas on 5/10/26.
+//
+
+import SwiftData
+
+@Model
+class Vegetable {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
